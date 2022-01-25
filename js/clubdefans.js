cf170442e95989929e7a799c74c8fc8cf0a53f5d
Camila Paijes <cpaijes@gmail.com>
2022-01-25 17:48:55 -0300

    arreglo form

--- js/clubdefans.js
+++ js/clubdefans.js
@@ -1,32 +1,38 @@
 
 
-function Miembro (nombre, edad, email){
-    this.nombre= nombre;
-    this.edad = edad;
-    this.email=email;
-  
+class Miembro {
+    constructor(nombre, edad, email){
+        this.nombre= nombre;
+        this.edad = edad;
+        this.email=email;
+    }
 }
 
-let nombre = prompt("Ingrese su nombre");
-let edad = prompt("Ingrese su edad");
-let email = prompt("Ingrese su email"); 
 
-var miembro1 = new Miembro (nombre, edad, email);
-console.log (miembro1);
+//let nombre = prompt("Ingrese su nombre");
+//let edad = prompt("Ingrese su edad");
+//let email = prompt("Ingrese su email"); 
 
-alert ("Bienvenido, " + nombre + "al club de fans de Star Wars!")
+//var miembro1 = new Miembro (nombre, edad, email);
+//console.log (miembro1);
 
-let personajePrimero = prompt("Ingrese su personaje favorito");
-let personajeSegundo = prompt("Ingrese su segundo personaje favorito");
-let personajeTercero = prompt ("Ingrese su tercer personaje favorito");
+//alert ("Bienvenido, " + nombre + "al club de fans de Star Wars!")
 
-var personajes = [personajePrimero, personajeSegundo, personajeTercero];
+//let personajePrimero = prompt("Ingrese su personaje favorito");
+//let personajeSegundo = prompt("Ingrese su segundo personaje favorito");
+//let personajeTercero = prompt ("Ingrese su tercer personaje favorito");
 
-alert (personajes.sort())
+//var personajes = [personajePrimero, personajeSegundo, personajeTercero];
 
-const form = document.getElementById("formulario")
+//alert (personajes.sort())
 
-formulario.addEventListener("submit", datosFormulario);
+//let form = document.getElementById("formulario");
+//form.addEventListener("submit", function(e){
+   // e.preventDefault();
+//});
+
+let boton = document.getElementById("botonFormulario");
+boton.addEventListener("click", cargarMiembro);
 
 
 function datosFormulario(){
@@ -34,5 +40,19 @@ function datosFormulario(){
     let edadUsuario = document.getElementById("edad").value;
     let emailUsuario = document.getElementById("email").value;
     let miembro1 = new Miembro (nombre, edad, email);
-    console.log (miembto1);
+    console.log (miembro1);
+    mostrarMiembro(miembro1);
+}
+
+function mostrarMiembro (miembro) {
+    let form = document.getElementById("formulario");
+    formulario.innerHTML = "";
+
+    let saludoMiembro  = document.createElement("div");
+    saludoMiembro.className = "saludoMiembro";
+    saludoMiembro.innerHTML = `Gracias ${miembro.nombre} por suscribirte al club de fans! Recibirás novedad a través de ${miembro.cliente}`;
+
+    formulario.appendChild(saludoMiembro);
+
+
 }
