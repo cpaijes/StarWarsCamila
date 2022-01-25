7b8036897933e094c04606e1935474811a43a46c
Camila Paijes <cpaijes@gmail.com>
2022-01-25 18:28:59 -0300

    saga en js

---
+++ js/saga.js
@@ -0,0 +1,82 @@
+let saga = [
+    {
+        id:1,
+        nombre: "Episodio 4",
+        año: 1977,
+        Sinópsis: "Los gobernantes del Imperio Galáctico, la fuerza del mal que domina el universo, capturan a la princesa Leia, quien posee datos confidenciales sobre su centro de operaciones militares, denominado Estrella de la Muerte. Antes de su secuestro, la Princesa transfiere la información ultrasecreta a la base de datos del robot R2-D2. Catalogado como un material inservible, R2-D2, junto con su compañero C3PO, son deportados a un planeta remoto, siendo comprados por el joven Luke Skywalker en el mercado negro. Debido a un hecho fortuito, Luke accede al mensaje de la princesa Leia, quien solicita el auxilio del veterano Caballero Jedi Obi-Wan Kenobi. Luke, Obi-Wan, los robots, el piloto Han Solo y el gigantesco wookiee Chewbacca conforman la tripulación que intentará liberar a la princesa Leia de los dominios de Darth Vader, la autoridad suprema del Imperio Galáctico.",
+        imagen: "./Imágenes/episodio4.jpg",
+    },
+    {
+        id:2,
+        nombre: "Episodio 5",
+        año: 1980,
+        Sinópsis: "Tras un ataque sorpresa de las tropas imperiales a las bases camufladas de la alianza rebelde, Luke Skywalker, en compañía de R2D2, parte hacia el planeta Dagobah en busca de Yoda, el último maestro Jedi, para que le enseñe los secretos de la Fuerza. Mientras, Han Solo, la princesa Leia, Chewbacca, y C3PO esquivan a las fuerzas imperiales y piden refugio al antiguo propietario del Halcón Milenario, Lando Calrissian, en la ciudad minera de Bespin, donde les prepara una trampa urdida por Darth Vader.",
+        imagen: "./Imágenes/episodio5.jpg",
+    },
+    {
+        id:3,
+        nombre: "Episodio 6",
+        año: 1997,
+        Sinópsis: "Luke Skywalker tenía un claro objetivo: acabar con el imperio y con su ya conocido padre Darth Vader. Lo tenía crudo, aunque sin Yoda no hubiera ido a ninguna parte. Se mantenía vivo porque tenía un objetivo claro, no como el resto de la humanidad. En cuanto dejas de tener objetivos y metas, te atrapa el lado oscuro. Eso debió pensar Luke, en el momento decisivo de enfrentarse a su progenitor. Han Solo, Leia, C3P0, R2D2, Chewacca y Lando Calrissian se mostrarán como el principal punto de apoyo para lograr el objetivo. ¿Por qué? Porque la amistad es lo más importante que te queda en este mundo, sin tus verdaderos amigos no conseguirás salir a conocer mundo y a aprender en cada momento de tus decisiones. El retorno del Jedi te enseñara el camino para gozar e interiorizar la Fuerza. La Fuerza te ayudará a vencer tus males.",
+        imagen: "./Imágenes/episodio6.jpg",
+    },
+    {
+        id:4,
+        nombre: "Episodio 1",
+        año: 1999,
+        Sinópsis: "Ambientada treinta años antes que “La guerra de las galaxias” (1977), muestra la infancia de Darth Vader, el pasado de Obi-Wan Kenobi y el resurgimiento de los Sith, los caballeros Jedi dominados por el Lado Oscuro. La Federación de Comercio ha bloqueado el pequeño planeta de Naboo, gobernado por la joven Reina Amidala; se trata de un plan ideado por Sith Darth Sidious, que, manteniéndose en el anonimato, dirige a los neimoidianos, que están al mando de la Federación. El Jedi Qui-Gon Jinn y su aprendiz Obi-Wan Kenobi convencen a Amidala para que vaya a Coruscant, la capital de la República y sede del Consejo Jedi, y trate de neutralizar esta amenaza. Pero, al intentar esquivar el bloqueo, la nave real resulta averiada, viéndose así obligada la tripulación a aterrizar en el desértico y remoto planeta de Tatooine.",
+        imagen: "./Imágenes/episodio1.jpeg",
+    },
+    {
+        id:5,
+        nombre: "Episodio 2",
+        año: 2002,
+        Sinópsis: "Diez años después del conflicto entre la Federación de Comercio y el planeta Naboo, la República continúa sumida en el caos. Para ayudar a los jedis en su lucha, el canciller supremo Palpatine, crea el ejército de los clones. Por otro lado, Anakin, que sigue las enseñanzas de Obi-Wan Kenobi, se enamora de Padmé Amidala, la antigua reina de Naboo que es ahora senadora. Padmé vive bajo la amenaza de unos asesinos que Anakin y Obi-Wan deben desenmascarar.",
+        imagen: "./Imágenes/episodio2.jpg",
+    },
+
+    {
+        id:6,
+        nombre: "Episodio 3",
+        año: 2005,
+        Sinópsis: "La Guerra de los Clones causa furor. Existe una clara hostilidad entre el Canciller Palpatine y el Consejo Jedi. Anakin Skywalker, joven Caballero Jedi entre la espada y la pared, duda sobre qué camino seguir. Seducido por la promesa de un poder sin parangón, tentado por el lado oscuro de la Fuerza, jura lealtad al maléfico Darth Sidious y se convierte en Darth Vader. Los Señores Sith unen sus fuerzas para preparar su revancha, que comenzará con el exterminio de los Jedi. Los únicos supervivientes de la masacre, Yoda y Obi Wan se lanzan a la búsqueda de los Sith. La persecución acaba con un espectacular combate de sables entre Anakin y Obi Wan, que decidirá el futuro de la galaxia.",
+        imagen: "./Imágenes/episodio3.jpg",
+    },
+    {
+        id:7,
+        nombre: "Episodio 7",
+        año: 2015,
+        Sinópsis: "Treinta años después de la victoria de la Alianza Rebelde sobre la segunda Estrella de la Muerte (hechos narrados en el Episodio VI: El retorno del Jedi), la galaxia está todavía en guerra. Una nueva República se ha constituido, pero una siniestra organización, la Primera Orden, ha resurgido de las cenizas del Imperio Galáctico. A los héroes de antaño, que luchan ahora en la Resistencia, se suman nuevos héroes: Poe Dameron, un piloto de caza, Finn, un soldado desertor de la Primera Orden, Rey, una joven chatarrera, y BB-8, un androide rodante. Todos ellos luchan contra las fuerzas del Mal: el Capitán Phasma, de la Primera Orden, y Kylo Ren, un temible y misterioso personaje que empuña un sable de luz roja.",
+        imagen: "./Imágenes/episodio7.jpg",
+    },
+    {
+        id:8,
+        nombre: "Episodio 8",
+        año: 2017,
+        Sinópsis: "Una nueva aventura está a punto de comenzar, antiguos y nuevos personajes además de viejas caras conocidas, se preparan para lo que está por llegar, pues arcaicos secretos de la Fuerza e impactantes revelaciones del pasado serán revelados. Esta octava entrega de la saga espacial comienza inmediatamente después de los hechos ocurridos al final de Star Wars: El despertar de la fuerza (2015). En ella se plantean muchas incógnitas para los protagonistas. ¿Se convertirá la joven chatarrera Rey, en Jedi tras ver que es sensible a la Fuerza? ¿Será el legendario Maestro Jedi Luke Skywalker, exiliado a un lejano lugar de la galaxia, quien enseñe a Rey los designios de la Fuerza? ¿Cómo afrontará la General de la Resistencia Leia Organa la muerte de Han Solo a manos de su hijo? ¿Qué hará ahora Poe Dameron que está libre de las garras de la Primera Orden?",
+        imagen: "./Imágenes/episodio8.jpg",
+    },
+    {
+        id:9,
+        nombre: "Episodio 9",
+        año: 2019,
+        Sinópsis: "Último capítulo de la tercera trilogía de la franquicia Star Wars. En esta novena entrega de la saga galáctica la historia llega a su fin con un viaje épico y alucinante hacia una galaxia muy, muy lejana. La Resistencia está bajo mínimos, pero en su lucha sin descanso ha contactado con aliados a lo largo y ancho de la galaxia para crear una red lo suficientemente potente para destruir a la Primera Orden. Este será el enfrentamiento final entre Rey (Daisy Ridley) y Kylo Ren (Adam Driver). Nacerán nuevas leyendas y la batalla final por la libertad está a punto de llegar.",
+        imagen: "./Imágenes/episodio9.jpg",
+    },
+]
+
+const contenedor = document.getElementById("container");
+saga.forEach((pelicula, indice)=>{
+    let card = document.createElement("div");
+    card.classList.add("card", "col-sm-12", "col-lg-4");
+    card.innerHTML = `
+    <img src="${pelicula.imagen}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${pelicula.nombre}</h5>
+      <p class="card-text">${pelicula.año}</p>
+      <p class="card-text">${pelicula.Sinópsis}</p>
+
+    </div>
+      `;
+    contenedor.appendChild(card);
+    })
